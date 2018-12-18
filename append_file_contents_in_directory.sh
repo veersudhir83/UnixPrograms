3503c5f6b842bcc3b5896eb44b40d4d82f2c368e
veersudhir83 <veersudhir83@gmail.com>
2018-12-18 15:43:48 +0530

    Modified the script

--- append_file_contents_in_directory.sh
+++ append_file_contents_in_directory.sh
@@ -4,14 +4,15 @@
 
 source_folder=$1
 output_file=$2
-dd if=/dev/null of=$output_file   # or echo > $output_file or cp /dev/null $output_file or truncate -s 0 $output_file
 
 if [ $# -eq 2 ]
 then
+    dd if=/dev/null of=$output_file   # or echo > $output_file or cp /dev/null $output_file or truncate -s 0 $output_file
     for file_name in `ls -al $source_folder/*.txt | awk '{print $9}'`
     do
         cat $file_name >> $output_file
     done
 else
-    "Provided source folder and output file name as arguments"
+    echo "\033[7mPlease provide source folder and output file name as arguments\033[0m"
+    echo "Example: sh script_name.sh source_folder output_file"
 fi
