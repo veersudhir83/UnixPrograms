5d6d3b14767402642b4f19d091583508b10fa929
veersudhir83 <veersudhir83@gmail.com>
2018-12-19 15:12:47 +0530

    updates to logical operations

--- logical_operations.sh
+++ logical_operations.sh
@@ -4,9 +4,26 @@
 
 echo "\033[7mInput a number between 1 and 100:\033[0m \c"
 read num
-if [ $num -ge 1 -a $num -le 100 ] 
+
+# use any of the below syntaxes
+# if [ $num -ge 1 -a $num -le 100 ] # -o for OR operator
+if [ $num -ge 1 ] && [ $num -le 100 ] # || for OR operator
 then
     echo "The number is in the given range."
 else
     echo "You crossed the range. Please try again."
+fi
+
+echo "\033[7mInput a char:\033[0m \c"
+read char
+if [ `echo $char | wc -c` -eq 2 ]
+then
+    if [ $char = a -o $char = A -o $char = e -o $char = E -o $char = i -o $char = I -o $char = o -o $char = O -o $char = u -o $char = U ]
+    then
+        echo "You entered a vowel."
+    else
+        echo "You entered a consonant."
+    fi
+else
+    echo "Invalid Input..."
 fi
