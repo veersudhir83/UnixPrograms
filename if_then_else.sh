4c8a17612ddc557399acfffbf6730f3420be50e9
veersudhir83 <veersudhir83@gmail.com>
2018-12-19 12:27:30 +0530

    Changes to existing script

--- if_then_else.sh
+++ if_then_else.sh
@@ -4,12 +4,13 @@
 
 echo "\033[7mEnter a number\033[0m"
 read num
-if [ $num -gt 0 ]
+if [ $num -eq 0 ]
 then
-    echo "It is a positive number"
-elif [ $num -eq 0 ]
+    echo "It is Zero"
+elif [ $num -ge 0 ]
 then
-    echo "num is equal to zero"
-else
+    echo "It is a positive integer"
+elif [ $num -lt 0 ]
+then
     echo "It is a negative integer"
 fi
