5ff83b09570ebb4def8d8ea52febb07bfe77be98
Sudheer Veeravalli <veersudhir83@gmail.com>
2019-01-11 12:34:14 +0530

    Create custom_functions.sh

---
+++ custom_functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Purpose: To store re-usable functions that can be loaded into memory and re-used from the shell prompt directly.
+# Exection: "chmod +x custom_functions.sh && . custom_functions.sh"
+
+goodmorning()
+{
+    echo "Good Morning..."
+}
+
+goodbye()
+{
+    echo "Good Bye..."
+}
