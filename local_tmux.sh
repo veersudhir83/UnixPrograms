b649c25f99e0e17fc0402030cc9840ba52aab96e
sudheer veeravalli <veersudhir83@gmail.com>
2019-02-09 10:00:51 +0530

    Added local tmux file

---
+++ local_tmux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sessionname="tmux_local"
+
+cd="cd ~/workbench"
+watch="watch -d -n 0.5 "
+pane1="$cd"
+pane2="$cd/UnixPrograms"
+pane3="top"
+pane4="cal"
+
+tmux kill-session -t $sessionname
+
+tmux set-option remain-on-exit on
+
+# Pane 1 settings
+tmux new-session -d -s $sessionname 
+tmux send-keys "$pane1" C-m
+
+# Pane 2 settings
+tmux split-window -p 25 -v
+tmux send-keys "$pane2" C-m
+
+# Pane 3 settings
+tmux split-window -p 25 -h
+tmux send-keys "$pane3" C-m
+
+# Pane 4 settings
+tmux split-window -p 25 -h
+tmux send-keys "$pane4" C-m
+
+tmux -2 attach-session -d -t $sessionname 
