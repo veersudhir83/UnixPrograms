9b14504ca26194a4fbb7ad024888a251f3c3835b
sudheer veeravalli <veersudhir83@gmail.com>
2019-02-16 22:27:44 +0530

    adding custom functions and tmux panes

--- custom_functions.sh
+++ custom_functions.sh
@@ -2,12 +2,17 @@
 # Purpose: To store re-usable functions that can be loaded into memory and re-used from the shell prompt directly.
 # Exection: "chmod +x custom_functions.sh && . custom_functions.sh"
 
-goodmorning()
+saygm()
 {
     echo "Good Morning..."
 }
 
-goodbye()
+saybye()
 {
     echo "Good Bye..."
+}
+
+linesep() 
+{
+    echo "---------------------------------------------------------------------------------------------------------"
 }
--- tmux_local.sh
+++ tmux_local.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# Purpose: To split into panes using tmux.
+# Execution: "./tmux_local.sh"
 
-sessionname="tmux_local"
+sessionname="local"
 
 cd="cd ~/workbench"
 watch="watch -d -n 0.5 "
 
 pane1="ssh osboxes@mydevops.io"
-pane2="$cd; ssh-add ~/.ssh/github_id_rsa; clear"
+pane2="$cd; ssh-add ~/.ssh/github_id_rsa; cd UnixPrograms; chmod +x custom_functions.sh && . custom_functions.sh; $cd; clear"
 pane3="ssh user@machineIp"
 pane4="cal"
 
@@ -19,12 +21,12 @@ tmux new-session -d -s $sessionname
 tmux send-keys "$pane1" C-m
 
 # Pane 2 settings
-tmux split-window -p 25 -v
+tmux split-window -p 20 -v
 tmux send-keys "$pane2" C-m
 
 # Pane 3 settings
-tmux split-window -p 25 -h
-tmux send-keys "$pane3" C-m
+# tmux split-window -p 25 -h
+# tmux send-keys "$pane3" C-m
 
 # Pane 4 settings
 tmux split-window -p 15 -h
